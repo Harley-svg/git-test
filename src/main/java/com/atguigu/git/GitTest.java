8618aff8357034c113fd89022a88b65c2d4059c4
Harley <fenglinhai80@gmail.com>
2022-10-13 11:13:11 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git33");
         System.out.println("hello git hot-fix");
+        System.out.println("hello master-test");
+
     }
 }
